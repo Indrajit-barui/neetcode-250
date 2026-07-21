a8502beb59b20cb3aa810d2889d87232002e3ed7
Indrajit barui <indrajitbarui67@gmail.com>
2026-07-21 21:52:52 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int right=people.size()-1;
+        int left=0;
+        int boat=0;
+        while(left<=right){
+            if(people[right]+people[left]<=limit){
+                boat++;
+                left++;
+                right--;
+            }
+            else if(people[right]<=limit){
+                boat++;
+                right--;
+            }
+            else if(people[left]<=limit){
+                boat++;
+                left++;
+            }
+        }
+       
+        return boat;
+    }
+};
